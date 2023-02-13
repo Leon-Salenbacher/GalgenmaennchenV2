1ec37d1d4ccfe0661c362282f23e16b381986bd8
Leon-BoU <leon@salenbacher.com>
2023-02-13 11:32:54 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "current state""""""
    This reverts commit fdf088c415d69368f6a6ec5f50828c21d7983ffd.

--- GalgenmaennchenV2/Service/RatingAdapter.cs
+++ GalgenmaennchenV2/Service/RatingAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using GalgenmaennchenV2.Objects;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,11 @@ namespace GalgenmaennchenV2.Service
 
         public void createRating(int userId, int fails, string word)
         {
+            //needed???
             Rating rating = new Rating(
                 fails,
                 word,
-                userId
+                new User()
                 );
 
             string sql = "INSERT INTO `galgenmaenchen`.`tbl_ratings` " +
@@ -25,11 +27,27 @@ namespace GalgenmaennchenV2.Service
             dbConnector.ExecuteNonQuery(sql);
         }
 
-        public Rating getHighScore(string userId)
+        public Rating getHighScore(int userId)
         {
-            return null;
+            string sql = "SELECT r.ID as ratingId, MIN(r.versuche) as versuche, r.word, u.ID as userId, u.name " +
+                    "FROM galgenmaenchen.tbl_ratings as r " +
+                    "INNER JOIN tbl_users as u ON u.ID = r.tbl_users_ID " +
+                    "WHERE u.ID = " + userId + " LIMIT 1;";
 
+            MySqlDataReader dataReader = dbConnector.ExecuteQuery(sql);
 
+            Rating highScore = new Rating(
+                    dataReader.GetInt32("ratingId"),
+                    dataReader.GetInt32("versuche"),
+                    dataReader.GetString("word"),
+                    new User(
+                            dataReader.GetInt32("userId"),
+                            dataReader.GetString("name")
+                        )
+                );
+
+            dataReader.Close();
+            return highScore;
         }
     }
 }
---
+++ galgenmaennchenTest/TestService/TestRatingAdapter.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalgenmaennchenV2.Service;
+using GalgenmaennchenV2.Objects;
+using GalgenmaennchenV2.Datenbanken;
+
+namespace galgenmaennchenTest.TestService
+{
+    [TestClass]
+    public class TestRatingAdapter
+    {
+
+        [TestMethod]
+        public void Test_GetHighScore_goodcase01()
+        {
+            RatingAdapter ratingAdapter = new RatingAdapter();
+
+            ratingAdapter.getHighScore(1);
+        }
+
+        [TestMethod]
+        public void create_Rating_goodcase01()
+        {
+            RatingAdapter ratingAdapter = new RatingAdapter();
+            ratingAdapter.createRating(1, 2, "TESTWORD2");
+        }
+
+        [TestMethod]
+        public void getHighScore()
+        {
+            RatingAdapter ratingAdapter = new RatingAdapter();
+            Rating highscore = ratingAdapter.getHighScore(1);
+            
+                
+                }
+
+
+
+        [TestMethod]
+        public void Test_Databaseconnection_goodcase01()
+        {
+            MySqlConnector dbConnector = new MySqlConnector("galgenmaennchen");
+            dbConnector.TestConnection();
+
+        }
+    }
+}
