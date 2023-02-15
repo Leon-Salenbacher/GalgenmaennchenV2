e668ead8a6d5930baa702f2929da70f12cf8e5fc
Leon-BoU <leon@salenbacher.com>
2023-02-15 10:09:16 +0100

    userID in programm

--- GalgenmaennchenV2/Program.cs
+++ GalgenmaennchenV2/Program.cs
@@ -16,7 +16,7 @@ namespace GalgenmaennchenV2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GamePage());
+            Application.Run(new GamePage(1));
         }
     }
 }
